e7e98c6303ab81a4b67ceaf4bc798f46110097f8
mtranter <mark.tranter@woolworths.com.au>
2024-10-07 16:29:19 +1100

    Fix tailwind css

--- example-v3/tailwind.config.ts
+++ example-v3/tailwind.config.ts
@@ -2,8 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../examples/shared/**/*.{jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
